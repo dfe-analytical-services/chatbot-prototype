53c416db3b137fcfbd4910361c7188cc2af96913
joesharratt1229 <joesharratt1229@gmail.com>
2023-08-23 10:29:21 +0100

    Adhered styling to python conventions

--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -1,44 +1,42 @@
-from typing import Awaitable, AsyncIterable
 import asyncio
-import json
+import logging
+from typing import AsyncIterable, Awaitable
+
+import openai
 import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse, StreamingResponse
 from langchain.callbacks.streaming_aiter import AsyncIteratorCallbackHandler
 from langchain.docstore.document import Document
 from pydantic import BaseModel
-import openai
-from dotenv import load_dotenv
 from qdrant_client import QdrantClient
-from starlette.exceptions import HTTPException 
+from starlette.exceptions import HTTPException
 from utils import makechain
-from fastapi.middleware.cors import CORSMiddleware
-import logging
 
 load_dotenv()
 
 app = FastAPI()
 
 origins = [
-    "http://localhost",
-    "http://localhost:8080",
-   "http://localhost:3000",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-  allow_methods=["*"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
 async def send_message(message: str) -> AsyncIterable[str]:
     callback = AsyncIteratorCallbackHandler()
-    client = QdrantClient('localhost', port = 6333)
-    embeds = openai.Embedding.create(input = message, engine = 'text-embedding-ada-002')
-    
+    client = QdrantClient("localhost", port=6333)
+    embeds = openai.Embedding.create(input=message, engine="text-embedding-ada-002")
+
     chain = makechain(callback)
 
     async def wrap_done(fn: Awaitable, event: asyncio.Event):
@@ -51,37 +49,37 @@ async def send_message(message: str) -> AsyncIterable[str]:
         finally:
             # Signal the aiter to stop.
             event.set()
-    
-    #query the database
-    resp = client.search(collection_name = 'ees', query_vector = embeds['data'][0]['embedding'],
-                         limit = 6)
-    
+
+    # query the database
+    resp = client.search(collection_name="ees", query_vector=embeds["data"][0]["embedding"], limit=6)
+
     logging.info(resp)
 
-    documents = [Document(page_content = resp[i].payload['text']) for i in range(len(resp))]
-    list_urls = list(set(resp[i].payload['url'] for i in range(len(resp))))
-    
-    task = asyncio.create_task(wrap_done(
-        chain.arun(input_documents = documents, question = message, links = str(list_urls)),
-        callback.done),
-    )
-    
-    #Logic to send the response to the frontend as an event stream
-    async for token in callback.aiter():
-        yield f'{token}'
+    documents = [Document(page_content=resp[i].payload["text"]) for i in range(len(resp))]
+    list_urls = list(set(resp[i].payload["url"] for i in range(len(resp))))
 
-    #yield json.dumps({'sourceDocuments': list_urls})
+    task = asyncio.create_task(
+        wrap_done(chain.arun(input_documents=documents, question=message, links=str(list_urls)), callback.done),
+    )
+
+    # Logic to send the response to the frontend as an event stream
+    async for token in callback.aiter():
+        yield f"{token}"
+
+    # yield json.dumps({'sourceDocuments': list_urls})
 
     await task
-    
+
+
 @app.exception_handler(HTTPException)
 async def http_exception_handler(exc: HTTPException):
-    return JSONResponse(status_code=exc.status_code, content={'error': exc.detail}) 
+    return JSONResponse(status_code=exc.status_code, content={"error": exc.detail})
 
 
-#pydantic validation of the request
+# pydantic validation of the request
 class StreamRequest(BaseModel):
     """Request body for streaming."""
+
     question: str
 
 
@@ -91,7 +89,8 @@ def stream(body: StreamRequest):
         return StreamingResponse(send_message(body.question), media_type="text/event-stream")
     except Exception as e:
         # ToDo: More specific error handling
-        raise HTTPException(status_code = 500, detail = str(e))
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 if __name__ == "__main__":
     uvicorn.run(host="0.0.0.0", port=8000, app=app)
--- backend/fastapi/utils.py
+++ backend/fastapi/utils.py
@@ -1,11 +1,9 @@
-import logging
-from langchain.chat_models import ChatOpenAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.callbacks import AsyncIteratorCallbackHandler
-from dotenv import load_dotenv
 import os
-from langchain.prompts.prompt import PromptTemplate
 
+from dotenv import load_dotenv
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.prompt import PromptTemplate
 
 load_dotenv()
 
@@ -14,32 +12,24 @@ a question and the following context relating to education information. Provide 
 If the question asked is not related to the education sector and not included in the contextual information, you MUST not answer the question and instead say Sorry this question does not relate to the service. 
 If and only if the question is related to the education sector say this information is not contained within the service
 but here are links to relevant publications. Parse the following links array and use as hyperlink if do this: {links}
-
 It is very important you only provide information relevant to the service and remain impartial and do not provide opinions, even if opinions
 are contained within the service. Where inferred by very specifc about dates of events (include year) not just month. 
 Question: {question}
- =========
+========
 {context}
-
-  =========
-
-  Answer in Markdown:
+=========
+Answer in Markdown:
 
 """
 
-QA_PROMPT = PromptTemplate(
-    template=prompt_template, input_variables=["context", "question", 'links']
-)
+QA_PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question", "links"])
+
 
 def makechain(callback) -> load_qa_chain:
     model = ChatOpenAI(
-        streaming=True,
-        verbose=True,
-        callbacks=[callback],
-        model='gpt-4',
-        openai_api_key=os.getenv('OPENAI_API_KEY')
+        streaming=True, verbose=True, callbacks=[callback], model="gpt-4", openai_api_key=os.getenv("OPENAI_API_KEY")
     )
 
-    chain = load_qa_chain(llm=model, chain_type='stuff', prompt = QA_PROMPT)
+    chain = load_qa_chain(llm=model, chain_type="stuff", prompt=QA_PROMPT)
 
     return chain
