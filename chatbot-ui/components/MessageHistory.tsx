8e5bbecf761d2f30c6c9e2a572ea901892d720f1
Amy Benson <amybenson@hiveit.co.uk>
2023-12-13 16:58:11 +0000

    EES-4727 tidy message styling

--- chatbot-ui/components/MessageHistory.tsx
+++ chatbot-ui/components/MessageHistory.tsx
@@ -4,7 +4,7 @@ import RobotIcon from '@/public/assets/images/icons/robot.svg';
 import UserIcon from '@/public/assets/images/icons/user.svg';
 import classNames from 'classnames';
 import ReactMarkdown from 'react-markdown';
-import { Message, MessageType } from '@/hooks/useChatbot';
+import { Message } from '@/hooks/useChatbot';
 
 type Props = {
   messages: Message[];
@@ -13,18 +13,6 @@ type Props = {
 
 // TODO: This could maybe be renamed
 const MessageHistory = ({ messages, loading }: Props) => {
-  const deriveCssClass = (messageType: MessageType, index: number): string => {
-    if (messageType === 'apiMessage') {
-      return styles.apimessage;
-    }
-
-    if (loading === true && index === messages.length - 1) {
-      return styles.usermessagewaiting;
-    }
-
-    return styles.usermessage;
-  };
-
   return (
     <div>
       <div className={styles.chat}>
@@ -32,23 +20,22 @@ const MessageHistory = ({ messages, loading }: Props) => {
           {messages.map((message, index) => (
             <div
               key={`chatMessage-${index}`}
-              className={classNames(deriveCssClass(message.type, index))}
+              className={classNames(styles.message, {
+                [styles.usermessagewaiting]:
+                  loading === true && index === messages.length - 1,
+                [styles.usermessage]: message.type === 'userMessage',
+              })}
               data-testid={
                 message.type === 'apiMessage' ? 'api-message' : 'user-message'
               }
             >
               {message.type === 'apiMessage' ? (
                 <RobotIcon
-                  className={styles.icon}
-                  height="1.1em"
+                  className={classNames(styles.icon, styles.robot)}
                   fill="#1d70b8"
                 />
               ) : (
-                <UserIcon
-                  className={styles.icon}
-                  height="1.1em"
-                  fill="#00703c"
-                />
+                <UserIcon className={styles.icon} fill="#00703c" />
               )}
 
               <div className={styles.markdownanswer}>
