e0cac9644e575e144447dbeeb556ae0ebfcc1ccf
Amy Benson <amybenson@hiveit.co.uk>
2023-12-13 16:29:43 +0000

    EES-4727 fix tests

--- chatbot-ui/pages/index.test.tsx
+++ chatbot-ui/pages/index.test.tsx
@@ -1,21 +1,11 @@
 import React from 'React';
 import { render, screen } from '@testing-library/react';
-import HomePage from './index';
-
-jest.mock('react-markdown');
-
-// TODO: Make whatever changes necessary to be able to test these components.
-// Currently, anything which uses react-markdown fails to compile with messages like:
-
-// export {Markdown as default, defaultUrlTransform} from './lib/index.js'
-// ^^^^^^
-//
-//  SyntaxError: Unexpected token 'export'
+import Home from './index';
 
 describe('Home Page', () => {
-  it.skip('Renders', () => {
-    render(<HomePage />);
+  it('Renders', () => {
+    render(<Home />);
 
-    expect(screen.getByRole('not-a-role')).toBeInTheDocument();
+    expect(screen.getByText('Chatbot prototype')).toBeInTheDocument();
   });
 });
