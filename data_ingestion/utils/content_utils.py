27d8983a69ca671ed5b1daa9be314aa2fb4519ea
Ben Outram <ben@hiveit.co.uk>
2024-01-12 13:21:52 +0000

    EES-4794 Fixing parsing of methodology content to handle content blocks without body text and include Annexe content.

--- data_ingestion/utils/content_utils.py
+++ data_ingestion/utils/content_utils.py
@@ -1,13 +1,27 @@
+import logging
+
 from bs4 import BeautifulSoup
 
+logger = logging.getLogger(__name__)
 
-def get_content_block_text(res: dict) -> str:
-    content_sections = res["content"]
-    result = "Content: "
-    for section_index in range(len(content_sections)):
-        content_blocks = content_sections[section_index]["content"]
-        for block_index in range(len(content_blocks)):
-            content_block = content_blocks[block_index]
-            if content_block["type"] == "HtmlBlock":
-                result += BeautifulSoup(markup=content_block["body"], features="html.parser").get_text()
-    return result
+
+def get_content(content_sections: list[dict]) -> str:
+    content_section_texts = list(filter(None, map(get_content_section_text, content_sections)))
+    if not content_section_texts:
+        return ""
+    return " ".join(content_section_texts)
+
+
+def get_content_section_text(content_section) -> str | None:
+    if content_section.get("content") is not None:
+        content_blocks = content_section["content"]
+        content_block_texts = list(filter(None, map(get_content_block_text, content_blocks)))
+        if len(content_block_texts) > 0:
+            section_heading = content_section["heading"]
+            return f"{section_heading}: " + " ".join(content_block_texts)
+
+
+def get_content_block_text(content_block) -> str | None:
+    if content_block["type"] == "HtmlBlock":
+        if content_block.get("body") is not None:
+            return BeautifulSoup(markup=content_block["body"], features="html.parser").get_text()
