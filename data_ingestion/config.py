51eb3235cdaa123ba424024b3a70985eff6b761d
Ben Outram <ben@hiveit.co.uk>
2024-01-15 15:36:32 +0000

    EES-4786 Move chat prompt template to Key vault secret and add an example prompt template to `.env.example`. (#74)

--- data_ingestion/config.py
+++ data_ingestion/config.py
@@ -4,17 +4,19 @@ from settus import BaseSettings, Field, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    model_config = SettingsConfigDict(
+        env_file=".env", extra="ignore", keyvault_url=os.getenv(key="AZURE_KEY_VAULT_ENDPOINT")
+    )
 
     ees_url_api_content: str = "http://localhost:5010/api"
     ees_url_api_data: str = "http://localhost:5000/api"
     ees_url_public_ui: str = "http://localhost:3000"
-    # TODO in a local environment without a key vault URL this variable should populate by name, i.e. from OPENAI_API_KEY but it doesn't work
-    # As a workaround the local .env file variable must match the alias name OPENAI-API-KEY which is the name in Azure key vault.
-    # This is different from the other environment variables which use snake case.
-    openai_api_key: str = Field(
-        default="placeholder-value", alias="openai-api-key", keyvault_url=os.getenv(key="AZURE_KEY_VAULT_ENDPOINT")
-    )
+    # TODO in a local environment without a key vault URL the following variable should populate by name,
+    # i.e. OPENAI_API_KEY but this doesn't work.
+    # As a workaround the local .env file variables must match the alias names
+    # i.e. OPENAI-API-KEY which are the names of the secrets in Azure key vault.
+    # This is different from the other environment variable names which all use snake case.
+    openai_api_key: str = Field(default="placeholder-value", alias="openai-api-key")
     openai_embedding_model: str = "text-embedding-ada-002"
     qdrant_collection: str = "ees"
     qdrant_host: str = "localhost"
--- response_automater/utils.py
+++ response_automater/utils.py
@@ -5,24 +5,7 @@ from pydantic import BaseModel
 
 from .config import settings
 
-prompt_template = """You are an AI assistant on the explore education statistics service. You are given
-a question, links and context which is used to answer the question. Provide an answer to the question based on the context provided. If any of the context
-provided isnt relevant to answer the question, it is imperative that you do not include this in your answer and stay focused on the question.
-If the question asked is not related to the education sector and not included in the contextual information, you MUST not answer the question and 
-instead say Sorry this question does not relate to the service. If and only if the question is related to the education sector 
-say this information is not contained within the service but here are links to relevant publications.Parse the following links array 
-and use hyperlink to direct the user to relevant pages on the service: {links} .It is very important you only provide 
-information relevant to the service and remain impartial and do not provide opinions, even if opinions are contained within the service. 
-Where inferred by very specific about dates of events (include year) not just month. 
-Question: {question}
-========
-{context}
-=========
-Answer in Markdown:
-
-"""
-
-QA_PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question", "links"])
+QA_PROMPT = PromptTemplate(template=settings.chat_prompt_template, input_variables=["context", "question", "links"])
 
 
 def makechain(callback) -> load_qa_chain:
