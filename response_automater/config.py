1eabd452006e90711a0847107045f89f9358b2a2
Ben Outram <ben@hiveit.co.uk>
2024-01-15 13:08:55 +0000

    EES-4786 Move chat prompt template to Key vault secret and add an example prompt template to `.env.example`.

--- response_automater/config.py
+++ response_automater/config.py
@@ -4,15 +4,18 @@ from settus import BaseSettings, Field, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore", populate_by_name=True)
+    model_config = SettingsConfigDict(
+        env_file=".env", extra="ignore", populate_by_name=True, keyvault_url=os.getenv(key="AZURE_KEY_VAULT_ENDPOINT")
+    )
 
     api_allow_origins: list[str] = ["http://localhost:3002"]
-    # TODO in a local environment without a key vault URL this variable should populate by name, i.e. from OPENAI_API_KEY but it doesn't work
-    # As a workaround the local .env file variable must match the alias name OPENAI-API-KEY which is the name in Azure key vault.
-    # This is different from the other environment variables which use snake case.
-    openai_api_key: str = Field(
-        default="placeholder-value", alias="openai-api-key", keyvault_url=os.getenv(key="AZURE_KEY_VAULT_ENDPOINT")
-    )
+    # TODO in a local environment without a key vault URL the following two variables should populate by name,
+    # i.e. CHAT_PROMPT_TEMPLATE and OPENAI_API_KEY but this doesn't work.
+    # As a workaround the local .env file variables must match the alias names
+    # i.e. CHAT-PROMPT-TEMPLATE and OPENAI-API-KEY which are the names of the secrets in Azure key vault.
+    # This is different from the other environment variable names which all use snake case.
+    chat_prompt_template: str = Field(default="placeholder-value", alias="chat-prompt-template")
+    openai_api_key: str = Field(default="placeholder-value", alias="openai-api-key")
     openai_model: str = "gpt-4"
     openai_embedding_model: str = "text-embedding-ada-002"
     qdrant_collection: str = "ees"
