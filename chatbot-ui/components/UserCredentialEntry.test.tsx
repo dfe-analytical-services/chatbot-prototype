7019815b0cb784e2e11c35f866dba16c32412488
Sam Biram <samuel.biram@airelogic.com>
2024-01-17 14:50:19 +0000

    Add test for UserCredentialEntry

---
+++ chatbot-ui/components/UserCredentialEntry.test.tsx
@@ -0,0 +1,71 @@
+import UserCredentialEntry from './UserCredentialEntry';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+const mockOnCorrectEntry = jest.fn();
+
+describe('User Credential Entry', () => {
+  beforeEach(() => {
+    render(
+      <UserCredentialEntry
+        onCorrectEntry={mockOnCorrectEntry}
+        authPassword="the-right-password"
+      />,
+    );
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  const getPasswordInput = () => {
+    return screen.getByLabelText('Please enter the password');
+  };
+
+  it('Renders', () => {
+    expect(
+      screen.getByRole('heading', { name: 'Please enter the password' }),
+    ).toBeVisible();
+  });
+
+  it('Autofocuses the first form field', () => {
+    expect(getPasswordInput()).toHaveFocus();
+  });
+
+  it('Allows the user to type a password', async () => {
+    await userEvent.type(getPasswordInput(), 'myPassword');
+
+    expect(getPasswordInput()).toHaveValue('myPassword');
+  });
+
+  it('Invokes the callback if entered password matches', async () => {
+    await userEvent.type(getPasswordInput(), 'the-right-password');
+
+    await userEvent.click(screen.getByRole('button', { name: 'Enter' }));
+
+    expect(mockOnCorrectEntry).toHaveBeenCalled();
+  });
+
+  it('Does not invoke the callback if entered password does not match', async () => {
+    await userEvent.type(getPasswordInput(), 'the-wrong-password');
+
+    await userEvent.click(screen.getByRole('button', { name: 'Enter' }));
+
+    expect(mockOnCorrectEntry).not.toHaveBeenCalled();
+  });
+
+  it('Displays a validation error if a password was not entered', async () => {
+    await userEvent.click(screen.getByRole('button', { name: 'Enter' }));
+
+    expect(screen.getByText('A password is required')).toBeVisible();
+  });
+
+  it('Displays an error message if the password was incorrect', async () => {
+    await userEvent.type(getPasswordInput(), 'the-wrong-password');
+
+    await userEvent.click(screen.getByRole('button', { name: 'Enter' }));
+
+    expect(screen.getByText('The password was incorrect')).toBeVisible();
+  });
+});
