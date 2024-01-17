61ef85dcda2ee3945973a571b5695f471c5419a7
Sam Biram <samuel.biram@airelogic.com>
2024-01-17 14:56:42 +0000

    Add tests for index page to prove transition works

--- chatbot-ui/components/UserCredentialEntry.test.tsx
+++ chatbot-ui/components/UserCredentialEntry.test.tsx
@@ -1,4 +1,4 @@
-import UserCredentialEntry from './UserCredentialEntry';
+import UserCredentialEntry from '@/components/UserCredentialEntry';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
