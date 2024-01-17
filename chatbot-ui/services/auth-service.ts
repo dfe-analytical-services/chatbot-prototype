82694c534bfa63f0960572298b3e097ed0a5879f
Sam Biram <samuel.biram@airelogic.com>
2024-01-17 18:08:47 +0000

    Remove experiments

--- chatbot-ui/services/auth-service.ts
+++ chatbot-ui/services/auth-service.ts
@@ -1,18 +1,13 @@
 import { headers } from 'next/headers';
 
 export const isAuthenticated = () => {
-  const foo = btoa('dfepassword');
-  //console.dir(`Foo: ${foo}`);
   const authorization = headers().get('authorization');
 
-  //console.dir(authorization);
   if (!authorization) {
     return false;
   }
 
   const authValue = authorization.split(' ')[1];
-  console.dir(atob(authValue));
-  console.dir(btoa('dfe:password'));
   const [username, password] = atob(authValue).split(':');
 
   return username == 'dfe' && password == process.env.AUTH_PASSWORD;
