cf26a4b6e2e0115a8a0378b7e2e9eab01050994d
Sam Biram <samuel.biram@airelogic.com>
2024-01-24 13:45:22 +0000

    Move tests out of pages directory

--- chatbot-ui/__tests__/index.test.tsx
+++ chatbot-ui/__tests__/index.test.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Home from '../pages/index';
 
 describe('Home Page', () => {
   it('Renders', () => {
     render(<Home apiUrl="https://localhost" authPassword="testPassword" />);
 
-    expect(screen.getByText('Support bot')).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Support bot' })).toBeVisible();
+  });
+
+  it('Shows the chatbot dialog once proper auth is entered', async () => {
+    render(<Home apiUrl={'some-url'} authPassword={'the-right-password'} />);
+
+    await userEvent.type(
+      screen.getByLabelText('Please enter the password'),
+      'the-right-password',
+    );
+
+    await userEvent.click(screen.getByRole('button', { name: 'Enter' }));
+
+    expect(screen.getByLabelText('What is your question?')).toBeVisible();
   });
 });
